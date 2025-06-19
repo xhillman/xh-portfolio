d3df28e361e12dbeb87d84873dea2770e3171f8c
Xavier Hillman <107809636+xhillman@users.noreply.github.com>
2025-06-19 18:29:33 +0000

    implement design style spacing and colors

---
+++ xh-port-next/src/app/_components/Header.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Header () {
+    return (
+        <header>
+            <h2>XAVIER HILLMAN</h2>
+            <nav>
+                <ul>
+                    <li>01 Home</li>
+                    <li>02 Projects</li>
+                    <li>03 About</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
