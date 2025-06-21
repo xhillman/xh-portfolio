4953e1f03f7fea0659ba13b7c9f70321563289d5
Xavier Hillman <xhillman13@gmail.com>
2025-06-20 23:58:16 -0400

    add custom corner bracket classes

--- xh-port-next/src/app/_components/Glyphs.tsx
+++ xh-port-next/src/app/_components/Glyphs.tsx
@@ -1,15 +1,17 @@
-
-
-export default function Glyphs () {
-    return (
-        <div>
-            <p className="text-8xl">1 &#x02df; &#x0f1d; &#x2b59; &#x2715; &#x002a; &#x2771;</p>
-            <p className="text-8xl">2 &#x2a00; &#x2981; &#x22a1; &#x22a0;</p>
-            <p className="text-8xl">3 &#x22ef; &#x2234; &#x2235;</p>
-            <p className="text-8xl">4 &#x22a2; &#x22a3; &#x22a4; &#x2adf; &#x22a5; &#x2ae0;</p>
-            <p className="text-8xl">5 &#x231C; &#x231d; &#x231e; &#x231f;</p>
-            <p className="text-8xl">6 &#x1f868; &#x1f869; &#x1f86a; &#x1f86b;</p>
-            <p className="text-8xl">7&#x1f86c; &#x1f86d; &#x1f86e; &#x1f86f;</p>
-        </div>
-    )
+export default function Glyphs() {
+  return (
+    <div>
+      <p className="text-8xl">
+        1 &#x02df; &#x0f1d; &#x2b59; &#x2715; &#x002a; &#x2771;
+      </p>
+      <p className="text-8xl">2 &#x2a00; &#x2981; &#x22a1; &#x22a0;</p>
+      <p className="text-8xl">3 &#x22ef; &#x2234; &#x2235;</p>
+      <p className="text-8xl">
+        4 &#x22a2; &#x22a3; &#x22a4; &#x2adf; &#x22a5; &#x2ae0;
+      </p>
+      <p className="text-8xl">5 &#x231C; &#x231d; &#x231e; &#x231f;</p>
+      <p className="text-8xl">6 &#x1f868; &#x1f869; &#x1f86a; &#x1f86b;</p>
+      <p className="text-8xl">7&#x1f86c; &#x1f86d; &#x1f86e; &#x1f86f;</p>
+    </div>
+  );
 }
--- xh-port-next/src/app/_components/Header.tsx
+++ xh-port-next/src/app/_components/Header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header () {
     return (
-        <header className="flex justify-between w-full p-2 md:p-4 relative box-border before:w-[16px] before:h-[16px] before:border-t-2 before:border-l-2 before:top-0 before:left-0 before:absolute before:border-white/25 after:w-[16px] after:h-[16px] after:top-0 after:right-0 after:border-t-2 after:border-r-2 after:absolute after:border-white/25">
+        <header className="flex justify-between w-full p-4 relative box-border t-corner-brackets b-corner-brackets bg-white/2">
             <h2 className="max-w-1/2"><span className="text-blue-500/50">*</span>XAVIER HILLMAN</h2>
             <nav>
                 <ul className="flex flex-col w-fit ">
