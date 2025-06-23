0f2ef6418e4cc523a23d08b8db7a6b0205e10d36
Xavier Hillman <107809636+xhillman@users.noreply.github.com>
2025-06-23 18:57:33 +0000

    add homepage style details

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,76 @@
+import Link from "next/link";
+import Glyphs from "./_components/Glyphs";
+
 export default function Home() {
   return (
-    <div id="home" className="box-border flex-auto p-4 relative h-[70vh] flex items-end">
+    <div id="home" className="box-border flex-auto p-4 relative h-[75dvh] flex items-end">
       <div className="intro box-border w-full">
-        <h1 className="text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-black">
-          
+        <h1 className="text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-black mb-2">
+
           SOFTWARE <br />
           DEVELOPER
         </h1>
-        <h2 className="max-w-[88%] md:max-w-[50%]">
+        <div className="subheading-callout relative p-2 t-corner-brackets b-corner-brackets md:p-2 md:w-full max-w-[88%] md:max-w-[75%] lg:max-w-[45%] mb-4 md:mb-4">
+        <h2 className="text-neutral-400 bg-[#171717] p-1 md:p-3 ">
           Digital solutions to real-world problems with React, TypeScript, and
           TailwindCSS.
         </h2>
+        </div>
+
+        {/* <Glyphs /> */}
+
+        <ul className="socials h-fit flex">
+          <li>
+            <Link href="" className="text-sm md:text-base font-bold flex mr-2 ease-in-out duration-200 hover:text-rose-500 hover:scale-105 hover:translate-x-1 hover:-translate-y-1">GitHub <svg
+                className="feather feather-arrow-up-right"
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="3"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              ><line x1="7" x2="17" y1="17" y2="7" />
+                <polyline points="7 7 17 7 17 17" />
+              </svg></Link>
+          </li>
+          <li>
+            <Link href="" className="text-sm md:text-base font-bold flex mr-2 ease-in-out duration-200 hover:text-rose-500 hover:scale-105 hover:translate-x-1 hover:-translate-y-1">LinkedIn<svg
+                className="feather feather-arrow-up-right"
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="3"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              ><line x1="7" x2="17" y1="17" y2="7" />
+                <polyline points="7 7 17 7 17 17" />
+              </svg></Link>
+          </li>
+          <li>
+            <Link href="" className="text-sm md:text-base font-bold flex ease-in-out duration-200 hover:text-rose-500 hover:scale-105 hover:translate-x-1 hover:-translate-y-1">Email<svg
+                className="feather feather-arrow-up-right"
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="3"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              ><line x1="7" x2="17" y1="17" y2="7" />
+                <polyline points="7 7 17 7 17 17" />
+              </svg></Link>
+          </li>
+        </ul>
       </div>
+      
     </div>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -16,11 +16,11 @@ export default function Projects() {
   return (
     <div
       id="projects"
-      className="box-border flex-auto flex flex-col gap-16 p-4 lg:flex-row relative "
+      className="box-border flex-auto flex flex-col gap-16 p-4 lg:flex-row relative"
     >
       <h1 className="text-5xl md:text-8xl lg:text-6xl xl:text-8xl font-black flex lg:items-end lg:sticky lg:top-0">PROJECTS</h1>
 
-      <div className="project-card-container flex-auto flex flex-col gap-8 items-center justify-center md:grid grid-cols-2 auto-rows-[1fr] lg:max-h-[85vh] lg:block sticky top-0 overflow-y-scroll lg:pr-6  lg:[&::-webkit-scrollbar]:w-1
+      <div className="project-card-container flex-auto flex flex-col gap-8 items-center justify-center pb-6 md:grid grid-cols-2 auto-rows-[1fr] lg:max-h-[85vh] lg:block sticky top-0 overflow-y-scroll lg:pr-6  lg:[&::-webkit-scrollbar]:w-1
   [&::-webkit-scrollbar-track]:rounded-full
   [&::-webkit-scrollbar-track]:bg-gray-100
   [&::-webkit-scrollbar-thumb]:rounded-full
@@ -28,22 +28,10 @@ export default function Projects() {
   dark:[&::-webkit-scrollbar-track]:bg-neutral-700
   dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
         {
-        projects.map((project) => {
-          return <ProjectCard key={project.projectName} project={project} />;
-        })
-        
-        }
-                {
-        projects.map((project) => {
-          return <ProjectCard key={project.projectName} project={project} />;
-        })
-        
-        }
-                        {
-        projects.map((project) => {
-          return <ProjectCard key={project.projectName} project={project} />;
-        })
-        
+          projects.map((project) => {
+            return <ProjectCard key={project.projectName} project={project} />;
+          })
+
         }
       </div>
     </div>
