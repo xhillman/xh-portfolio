f379cbad03630c1cc9f30872a2a56e776799b814
Xavier Hillman <107809636+xhillman@users.noreply.github.com>
2025-06-23 15:20:12 +0000

    style projects scrollbar

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,7 +20,13 @@ export default function Projects() {
     >
       <h1 className="text-5xl md:text-8xl lg:text-6xl xl:text-8xl font-black flex lg:items-end lg:sticky lg:top-0">PROJECTS</h1>
 
-      <div className="project-card-container flex-auto flex flex-col gap-8 items-center justify-center md:grid grid-cols-2 auto-rows-[1fr] lg:max-h-[85vh] lg:block sticky top-0 overflow-auto lg:pr-6">
+      <div className="project-card-container flex-auto flex flex-col gap-8 items-center justify-center md:grid grid-cols-2 auto-rows-[1fr] lg:max-h-[85vh] lg:block sticky top-0 overflow-y-scroll lg:pr-6  lg:[&::-webkit-scrollbar]:w-1
+  [&::-webkit-scrollbar-track]:rounded-full
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:rounded-full
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
         {
         projects.map((project) => {
           return <ProjectCard key={project.projectName} project={project} />;
