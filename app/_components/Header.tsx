7e5dd6f25d869f7cebe95eb6a8e4da43a50eaa21
Xavier Hillman <107809636+xhillman@users.noreply.github.com>
2025-06-23 19:36:12 +0000

    adjust menu micro interaction

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -9,61 +9,24 @@ export default function Header() {
       <nav className="">
         <ul className="flex flex-col h-full lg:flex-row justify-around lg:items-center">
           <li className="mb-4 lg:mb-0 lg:mr-4">
-            <Link href="/" className="flex hover:text-rose-500 hover:scale-105 hover:decoration-rose-500 hover:translate-x-1 hover:-translate-y-1">
+            <Link href="/" className="flex hover:text-rose-500 ease-in-out duration-200 hover:translate-x-1">
               
-                Home<svg
-                className="feather feather-arrow-up-right"
-                fill="none"
-                height="24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              ><line x1="7" x2="17" y1="17" y2="7" />
-                <polyline points="7 7 17 7 17 17" />
-              </svg>
+                Home &#x1f86a;
             </Link>
           </li>
           <li className="mb-4 lg:mb-0 lg:mr-4">
             <Link
               href="/projects"
-              className="flex hover:text-rose-500 hover:scale-105 hover:decoration-rose-500 hover:translate-x-1 hover:-translate-y-1"
+              className="flex hover:text-rose-500 ease-in-out duration-200 hover:translate-x-1"
             >
-              Projects <span><svg
-                className="feather feather-arrow-up-right"
-                fill="none"
-                height="24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              ><line x1="7" x2="17" y1="17" y2="7" />
-                <polyline points="7 7 17 7 17 17" /></svg></span>
+              Projects &#x1f86a;
             </Link>
           </li>
           <li className="">
           <Link
               href="/about"
-              className="flex hover:text-rose-500 hover:scale-105 hover:decoration-rose-500 hover:translate-x-1 hover:-translate-y-1"
-            >About<svg
-                className="feather feather-arrow-up-right"
-                fill="none"
-                height="24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              ><line x1="7" x2="17" y1="17" y2="7" />
-                <polyline points="7 7 17 7 17 17" /></svg>
+              className="flex hover:text-rose-500 ease-in-out duration-200 hover:translate-x-1"
+            >About &#x1f86a;
             </Link></li>
         </ul>
       </nav>
